3fab0f9fa1e7d3fba2895235dde681ea6579949d
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-03-23 19:40:59 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 import GameSavingLoader from './GameSavingLoader';
 
-GameSavingLoader.load().then((saving) => {
-  console.log(saving);
-});
+// GameSavingLoader.load().then((saving) => {
+//   console.log(saving);
+// });
+
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    console.log(saving);
+  } catch (e) {
+    console.error(e.mesage);
+  }
+})();
